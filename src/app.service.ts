4c3b4ea703743391bd4ce7f61458d00116ea8289
FranciscoBelda <francisco.belda@cieep.com>
2025-01-31 19:52:59 +0100

    v1.0 App Service Updated con endpoints

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,38 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      "endpoints": [
+        {
+          "method": "GET",
+          "url": "https://backend-movies.vercel.app/api/v1/movies/paginated?page=1&limit=10"
+        },
+        {
+          "method": "POST",
+          "url": "https://backend-movies.vercel.app/api/v1/movies"
+        },
+        {
+          "method": "GET",
+          "url": "https://backend-movies.vercel.app/api/v1/movies/movie/:id"
+        },
+        {
+          "method": "GET",
+          "url": "https://backend-movies.vercel.app/api/v1/movies/byName?name=LOQUEBUSCAS"
+        },
+        {
+          "method": "GET",
+          "url": "https://backend-movies.vercel.app/api/v1/movies/genres"
+        },
+        {
+          "method": "UPDATE",
+          "url": "https://backend-movies.vercel.app/api/v1/movies/update/:id"
+        },
+        {
+          "method": "DELETE",
+          "url": "https://backend-movies.vercel.app/api/v1/movies/delete/:id"
+        }
+      ]
+    };
   }
 }
