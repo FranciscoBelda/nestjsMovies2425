fed8bdcc9084fcae816c5153ccdcd16fd3e60cbd
Francisco Belda <118449691+FranciscoBelda@users.noreply.github.com>
2025-01-31 22:56:13 +0100

    Update app.service.ts added amber in path

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,31 +7,31 @@ export class AppService {
       "endpoints": [
         {
           "method": "GET",
-          "url": "https://backend-movies.vercel.app/api/v1/movies/paginated?page=1&limit=10"
+          "url": "https://backend-movies-amber.vercel.app/api/v1/movies/paginated?page=1&limit=10"
         },
         {
           "method": "POST",
-          "url": "https://backend-movies.vercel.app/api/v1/movies"
+          "url": "https://backend-movies-amber.vercel.app/api/v1/movies"
         },
         {
           "method": "GET",
-          "url": "https://backend-movies.vercel.app/api/v1/movies/movie/:id"
+          "url": "https://backend-movies-amber.vercel.app/api/v1/movies/movie/:id"
         },
         {
           "method": "GET",
-          "url": "https://backend-movies.vercel.app/api/v1/movies/byName?name=LOQUEBUSCAS"
+          "url": "https://backend-movies-amber.vercel.app/api/v1/movies/byName?name=LOQUEBUSCAS"
         },
         {
           "method": "GET",
-          "url": "https://backend-movies.vercel.app/api/v1/movies/genres"
+          "url": "https://backend-movies-amber.vercel.app/api/v1/movies/genres"
         },
         {
           "method": "UPDATE",
-          "url": "https://backend-movies.vercel.app/api/v1/movies/update/:id"
+          "url": "https://backend-movies-amber.vercel.app/api/v1/movies/update/:id"
         },
         {
           "method": "DELETE",
-          "url": "https://backend-movies.vercel.app/api/v1/movies/delete/:id"
+          "url": "https://backend-movies-amber.vercel.app/api/v1/movies/delete/:id"
         }
       ]
     };
